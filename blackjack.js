016a257b8bae0d4cab1476c641d94a74a7f00248
Jan Bialostok <janbialostok@gmail.com>
2015-01-20 14:57:31 -0500

    Prototype  build of js functions for blackjack game

---
+++ blackjack.js
@@ -0,0 +1,219 @@
+var deck_of_cards = new Blackjack();
+function Blackjack(){
+	this.topCard = undefined;
+	this.bottomCard = undefined;
+	this.totalCards = 0;
+	this.players = [
+		{dealer: [],
+		 name: "dealer",
+		 handsValue: 0
+		}
+	];
+	this.firstDeal = true;
+}
+
+Blackjack.prototype.resetGame = function(){
+	deck_of_cards = new Blackjack();
+	playerTotal = 1;
+	cardSpot = 1;
+}
+
+Blackjack.prototype.shuffle = function(){
+	var set_of_cards = [];
+	var set_of_suits = ['clubs','hearts','spades','diamonds'];
+	for (var i = 2; i < 15; i++){
+		for (var suit = 0; suit < 4; suit++){
+			if (i == 11){
+				set_of_cards.push("Jack " + set_of_suits[suit] + ",10");
+			}
+			else if (i == 12){
+				set_of_cards.push("Queen " + set_of_suits[suit] + ",10");
+			}
+			else if (i == 13){
+				set_of_cards.push("King " + set_of_suits[suit] + ",10");
+			}
+			else if (i == 14){
+				set_of_cards.push("Ace " + set_of_suits[suit] + ",11");
+			}
+			else{
+				set_of_cards.push(i + " " + set_of_suits[suit] + "," + i);
+			}
+		}
+	}
+	console.log(set_of_cards.length);
+	var alreadyUsed = [];
+	for (var i = 0; i < set_of_cards.length; i++){
+		var whichCard = Math.floor((Math.random() * 52) + 1);
+		if (alreadyUsed.length == 0){
+			alreadyUsed.push(whichCard - 1);
+			this.addToDeck(set_of_cards[whichCard - 1]);
+		}
+		else{
+			for (var x = 0; x < alreadyUsed.length; x++){
+				if ((whichCard - 1) == alreadyUsed[x]){
+					whichCard = Math.floor((Math.random() * 52) + 1);
+					x = 0;
+				}
+			}
+			alreadyUsed.push(whichCard - 1);
+			this.addToDeck(set_of_cards[whichCard - 1]);
+		}
+	}
+
+}
+
+Blackjack.prototype.addToDeck = function(val){
+	var splitCard = val.split(",");
+	console.log(val);
+	if (typeof this.topCard == "undefined" && typeof this.bottomCard == "undefined"){
+		this.topCard = new Card(splitCard[0], splitCard[1]);
+		this.bottomCard = this.topCard;
+		this.totalCards++;
+	}
+	else{
+		var previousCard = this.bottomCard;
+		this.bottomCard = new Card(splitCard[0], splitCard[1]);
+		previousCard.nextCard = this.bottomCard;
+		this.totalCards++;
+	}
+}
+
+function Card(val, numVal){
+	this.faceValue = val;
+	this.numericalValue = numVal;
+	this.nextCard = null;
+}
+
+Blackjack.prototype.dealCard = function(){
+	if (typeof this.topCard == "undefined"){
+		console.log("There are no cards in the deck");
+		return "There are no cards in the deck";
+	}
+	else{
+	    var currentCardObj = this.topCard;
+		var currentCard = this.topCard.faceValue;
+		var currentCardVal = this.topCard.numericalValue;
+		if (this.topCard.nextCard == null){
+			this.topCard = undefined;
+			this.bottomCard = undefined;
+		}
+		else{
+			this.topCard = this.topCard.nextCard;
+			this.totalCards--;
+		}
+		console.log(currentCard + " " + currentCardVal);
+		currentCardObj.nextCard = null;
+		return currentCardObj;
+	}
+}
+
+var playerTotal = 1;
+var cardSpot = 1;
+
+Blackjack.prototype.tallyHandValue = function(playerNum){
+    var playerArraySpot;
+    if (playerNum == "dealer"){
+        playerArraySpot = 0;
+    }
+    else{
+        playerArraySpot = playerNum;
+    }
+	var cardCount = this.players[playerArraySpot][playerNum].length;
+	var handValue = 0;
+	for (var i = 0; i < cardCount; i++){
+		handValue += Number(this.players[playerArraySpot][playerNum][i].numericalValue);
+	}
+	this.players[playerArraySpot].handsValue = handValue;
+}
+
+Blackjack.prototype.findNextPlayer = function(currentPlayer){
+	do { if (cardSpot >= this.players.length - 1){
+				cardSpot = 0;
+				currentPlayer = 0;
+				while (this.players[0].handsValue < 17){
+					this.assignCard();
+				}
+				this.endHand();
+			}
+		else{
+			cardSpot++;
+		 	currentPlayer++; 
+		} 
+	}
+	while (this.players[currentPlayer].hasHold === true)
+}
+
+Blackjack.prototype.endHand = function(){
+    for (var i = 1; i < this.players.length; i++){
+        if (this.players[i].handsValue > this.players[0].handsValue && this.players[i].handsValue <= 21 || (this.players[0].handsValue > 21 && this.players[i].handsValue <= 21)){
+            this.players[i].winLoss = "Win";
+        }
+        else if (this.players[i].handsValue == this.players[0].handsValue && this.players[i].handsValue <= 21){
+            this.players[i].winLoss = "Push";
+        }
+        else{
+            this.players[i].winLoss = "Loss";
+        }
+    }
+}
+
+Blackjack.prototype.assignCard = function(){
+	var playerCount = this.players.length;
+	if (this.firstDeal){
+		for (var i = 1; i <= playerCount; i++){
+		    if (i == playerCount){
+		        var x = "dealer";
+		        this.players[0][x].push(this.dealCard());
+		    }
+		    else{
+		        this.players[i][i].push(this.dealCard());    
+		    }
+		    
+		}
+		for (var y = 1; y <= playerCount; y++){
+		    if (y == playerCount){
+		        var z = "dealer";
+		        this.players[0][z].push(this.dealCard());
+		        this.tallyHandValue("dealer");
+		    }
+		    else{
+		    this.players[y][y].push(this.dealCard());
+		    this.tallyHandValue(y); 
+		    }
+		    
+		}
+		this.firstDeal = false;	
+	}
+	else{
+		if (cardSpot > 0){
+			this.players[cardSpot][cardSpot].push(this.dealCard());
+			this.tallyHandValue(cardSpot);
+			if (this.players[cardSpot].handsValue > 21){
+			    this.hold();
+			}
+		}
+		else{
+			this.players[0]["dealer"].push(this.dealCard());
+			this.tallyHandValue("dealer");
+		}
+	}
+}
+
+Blackjack.prototype.hold = function(){
+	this.players[cardSpot].hasHold = true;
+	this.findNextPlayer(cardSpot);
+}
+
+Blackjack.prototype.addPlayer = function(playerName){
+	var playerObject = new Player(playerTotal, playerName)
+	this.players.push(playerObject);
+	playerTotal++;
+}
+
+function Player(playerNum, playerName){
+	this[playerNum] = [];
+	this.name = playerName;
+	this.hasHold = false;
+	this.handsValue = 0;
+	this.winLoss;
+}
